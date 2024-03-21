12cb36807c92b07e8f6eff14c41c6a80603ce899
quan.doan <dvquan210502@gmail.com>
2024-03-21 21:11:04 +0700

    update solution

---
+++ Core/SchoolManagement.UI/Controls/PropertyGrid/PropertyGrid.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace SchoolManagement.UI.Controls.PropertyGrid
+{
+    public class PropertyGrid : Control
+    {
+        public static readonly StyledProperty<object> SelectedObjectProperty = AvaloniaProperty.Register<PropertyGrid, object>(nameof(SelectedObject));
+
+        public object SelectedObject { get; set; }
+    }
+}
