9da5728694c2d119bddb6d14fc2b9590bf2c06c9
quan.doan <dvquan210502@gmail.com>
2024-04-19 15:03:13 +0700

    add property editor base

---
+++ Core/SchoolManagement.UI/Controls/PropertyGrid/Editors/PropertyEditorBase.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+//using System.Drawing.Imaging;
+//using System.Linq;
+//using System.Reflection;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SchoolManagement.UI.Controls.PropertyGrid.Editors
+//{
+//    public abstract class PropertyEditorBase : AvaloniaObject
+//    {
+//        public abstract Control CreateElemement(PropertyInfo propertyInfo);
+//        public virtual void CreateBinding(PropertyInfo propertyInfo, AvaloniaObject element) =>
+//            BindingOperations.Apply(element, GetAvaloniaProperty(), InstancedBinding($"{propertyInfo.Name}")
+//            {
+//                Source = propertyInfo.GetValue(null),
+//                Converter = GetConverter(propertyInfo)
+//            },null);
+//        public abstract AvaloniaProperty GetAvaloniaProperty();
+//        protected virtual IValueConverter GetConverter(PropertyInfo propertyInfo) => null;
+//    }
+//}
