8e5aca4a0e9fc168d390d37583a9622cb1d58609
quan.doan <dvquan210502@gmail.com>
2024-04-22 16:48:27 +0700

    add dialog

---
+++ Core/SchoolManagement.UI/Dialogs/ProcessView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SchoolManagement.UI.Dialogs
+{
+    public partial class ProcessView : UserControl
+    {
+        public ProcessView()
+        {
+            InitializeComponent();
+        }
+    }
+}
